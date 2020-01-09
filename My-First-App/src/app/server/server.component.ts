cfd95c599d8052f858714449ef116280904c88d7
Long Du <long020396@gmail.com>
2020-01-09 22:19:16 +0700

    Styling elements dynamically using ngStyle

--- My-First-App/src/app/server/server.component.ts
+++ My-First-App/src/app/server/server.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html'
 })
 export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 
   getServerStatus() {
     return this.serverStatus;
