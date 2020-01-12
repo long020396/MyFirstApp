d9a243ac80273e8b0c4c455b80795dc62f2c1961
Long Du <long020396@gmail.com>
2020-01-12 13:31:04 +0700

    Outputting shopping list. Adding shopping list edit.

--- prj-start/src/app/app.module.ts
+++ prj-start/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingDetailComponent } from './shopping-list/shopping-detail/shopping-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,7 @@ import { ShoppingDetailComponent } from './shopping-list/shopping-detail/shoppin
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingDetailComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
