3a58df384e7f2bc05f9abe4dcf0cd027df788bfa
Long Du <long020396@gmail.com>
2020-02-28 01:17:23 +0700

    Passing static data to a route

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -10,29 +10,31 @@ import { UserComponent } from "./users/user/user.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    {
-        path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
-            { path: ':id', component: ServerComponent },
-            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
-        ]
-    },
-    {
-        path: 'users', component: UsersComponent, children: [
-            { path: ':id/:name', component: UserComponent }
-        ]
-    },
-    { path: 'not-found', component: PageNotFoundComponent },
-    { path: '**', redirectTo: '/not-found' }
+  { path: '', component: HomeComponent },
+  {
+    path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
+    ]
+  },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent }
+    ]
+  },
+  // { path: 'not-found', component: PageNotFoundComponent },
+  { path: 'not-found', component: ErrorPageComponent, data: { message: 'Page not found!' } },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes)
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
 })
 
 export class AppRoutingModule { }
